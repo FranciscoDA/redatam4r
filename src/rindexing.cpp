e48b8e2d00e43aea74ea09faa7aa95bf78e7d7d6
FranciscoDA <franciscoda@outlook.com>
2018-12-17 01:56:58 -0300

    prevent unnecessary seeks while indexing variables

--- src/rindexing.cpp
+++ src/rindexing.cpp
@@ -49,55 +49,13 @@ void configure_factor(SEXP dst, SEXP src) {
 	}
 }
 
-extern "C" {
-
-SEXP bin_get_rvector(SEXP bin, SEXP indices) {
-	std::string path = CHAR(asChar(STRING_ELT(getAttrib(bin, mkString("data.path")), 0)));
-	const int instance_len = INTEGER_ELT(getAttrib(bin, mkString("instance.len")), 0);
-	const int instance_num = INTEGER_ELT(getAttrib(bin, mkString("instance.num")), 0);
-
-	std::fstream f (path, std::ios_base::in | std::ios_base::binary);
-	if (!f)
-		error_return("could not open data file for reading");
-
-	ProtectedSEXP ans = allocVector(INTSXP, length(indices));
-
-	const uint32_t bits_per_word = sizeof(uint32_t) * 8;
-	const uint32_t mask = (1 << instance_len) - 1;
-	for (int dst_index = 0; dst_index < length(indices); ++dst_index) {
-		int src_index = INTEGER_ELT(indices, dst_index)-1;
-
-		if (src_index < 0 or src_index >= instance_num) {
-			SET_INTEGER_ELT(ans, dst_index, NA_INTEGER);
-			continue;
-		}
-
-		f.seekg(src_index * instance_len / bits_per_word * sizeof(uint32_t));
-		const int shifted_bits = src_index * instance_len % bits_per_word;
-
-		if (bits_per_word - shifted_bits >= instance_len) {
-			uint32_t element = fread_BIN(f);
-			element >>= sizeof(uint32_t) * 8 - instance_len - shifted_bits;
-			element &= mask;
-			SET_INTEGER_ELT(ans, dst_index, element);
-		}
-		else {
-			uint32_t high = fread_BIN(f);
-			uint32_t low = fread_BIN(f);
-			const int bits_in_low = instance_len + shifted_bits - bits_per_word;
-			high <<= bits_in_low;
-			low >>= bits_per_word - bits_in_low;
-			SET_INTEGER_ELT(ans, dst_index, (high+low) & mask);
-		}
-	}
-	configure_factor(ans, bin);
-	return ans;
-}
-
-SEXP pck_get_rvector(SEXP pck, SEXP indices) {
-	std::string path = CHAR(asChar(STRING_ELT(getAttrib(pck, mkString("data.path")), 0)));
-	const int instance_len = INTEGER_ELT(getAttrib(pck, mkString("instance.len")), 0);
-	const int instance_num = INTEGER_ELT(getAttrib(pck, mkString("instance.num")), 0);
+// reads a variable-bit object using the provided function in `fread_elt`
+// fread_elt should return a word
+template<typename F>
+SEXP variable_bit_get_rvector(SEXP robj, SEXP indices, F&& fread_elt) {
+	std::string path = CHAR(asChar(STRING_ELT(getAttrib(robj, mkString("data.path")), 0)));
+	const unsigned int instance_len = INTEGER_ELT(getAttrib(robj, mkString("instance.len")), 0);
+	const int instance_num = INTEGER_ELT(getAttrib(robj, mkString("instance.num")), 0);
 
 	std::fstream f (path, std::ios_base::in | std::ios_base::binary);
 	if (!f)
@@ -107,6 +65,9 @@ SEXP pck_get_rvector(SEXP pck, SEXP indices) {
 
 	const uint32_t bits_per_word = sizeof(uint32_t) * 8;
 	const uint32_t mask = (1 << instance_len) - 1;
+
+	uint32_t element = fread_elt(f);
+	size_t current_offset = 0;
 	for (int dst_index = 0; dst_index < length(indices); ++dst_index) {
 		int src_index = INTEGER_ELT(indices, dst_index)-1;
 
@@ -115,133 +76,120 @@ SEXP pck_get_rvector(SEXP pck, SEXP indices) {
 			continue;
 		}
 
-		f.seekg(src_index * instance_len / bits_per_word * sizeof(uint32_t));
+		size_t target_offset = src_index * instance_len/bits_per_word * sizeof(uint32_t);
+		// only use seek if we really need to (can be really slow, and contiguous indices don't need to seek)
+		if (target_offset != current_offset) {
+			f.seekg(target_offset);
+			element = fread_elt(f);
+			current_offset = target_offset;
+		}
 		const int shifted_bits = src_index * instance_len % bits_per_word;
 
 		if (bits_per_word - shifted_bits >= instance_len) {
-			uint32_t element = fread_PCK(f);
-			element >>= sizeof(uint32_t) * 8 - instance_len - shifted_bits;
-			element &= mask;
-			SET_INTEGER_ELT(ans, dst_index, element);
+			uint32_t v = element >> (sizeof(uint32_t) * 8 - instance_len - shifted_bits);
+			v &= mask;
+			SET_INTEGER_ELT(ans, dst_index, v);
 		}
 		else {
-			uint32_t high = fread_PCK(f);
-			uint32_t low = fread_PCK(f);
-			const int bits_in_low = instance_len + shifted_bits - bits_per_word;
-			high <<= bits_in_low;
-			low >>= bits_per_word - bits_in_low;
-			SET_INTEGER_ELT(ans, dst_index, (high+low) & mask);
+			uint32_t next_element = fread_elt(f);
+			const int bits_in_first = instance_len + shifted_bits - bits_per_word;
+			uint32_t v = (element << bits_in_first) + (next_element >> (bits_per_word-bits_in_first));
+			SET_INTEGER_ELT(ans, dst_index, v & mask);
+			element = next_element;
+			++current_offset;
 		}
 	}
-	configure_factor(ans, pck);
+	configure_factor(ans, robj);
 	return ans;
 }
 
-SEXP chr_get_rvector(SEXP chr, SEXP indices) {
-	std::string path = CHAR(asChar(STRING_ELT(getAttrib(chr, mkString("data.path")), 0)));
-	const int instance_len = INTEGER_ELT(getAttrib(chr, mkString("instance.len")), 0);
-	const int instance_num = INTEGER_ELT(getAttrib(chr, mkString("instance.num")), 0);
+// reads objects from the file using `fread_elt` and writes them to the result array using `write_elt`
+// `fread_elt` receives the file stream and the number of bytes per element
+// `write_elt` receives the destination array SEXP, the destination index and the value returned by `fread_elt`
+template<typename F, typename G, typename NAT>
+SEXP variable_byte_get_rvector(SEXP robj, SEXP indices, F&& fread_elt, G&& write_elt, NAT naval, int anstype) {
+	std::string path = CHAR(asChar(STRING_ELT(getAttrib(robj, mkString("data.path")), 0)));
+	const int instance_len = INTEGER_ELT(getAttrib(robj, mkString("instance.len")), 0);
+	const int instance_num = INTEGER_ELT(getAttrib(robj, mkString("instance.num")), 0);
 
 	std::fstream f (path, std::ios_base::in | std::ios_base::binary);
 	if (!f)
 		error_return("could not open data file for reading");
 
-	ProtectedSEXP ans = allocVector(STRSXP, length(indices));
+	ProtectedSEXP ans = allocVector(anstype, length(indices));
 
+	size_t current_offset = 0;
 	for (int dst_index = 0; dst_index < length(indices); ++dst_index) {
 		int src_index = INTEGER_ELT(indices, dst_index)-1;
 
 		if (src_index < 0 || src_index >= instance_num) {
-			SET_STRING_ELT(ans, dst_index, NA_STRING);
+			write_elt(ans, dst_index, naval);
 			continue;
 		}
 
-		f.seekg(src_index * instance_len);
-		std::string s = fread_fixed_string(f, instance_len);
-		rtrim(s);
-		SET_STRING_ELT(ans, dst_index, mkChar(s.c_str()));
+		size_t target_offset = src_index * instance_len;
+		if (target_offset != current_offset) {
+			f.seekg(target_offset);
+			current_offset = target_offset;
+		}
+		auto v = fread_elt(f, instance_len);
+		write_elt(ans, dst_index, v);
+		current_offset += instance_len;
 	}
 	return ans;
 }
 
+extern "C" {
+
+SEXP bin_get_rvector(SEXP bin, SEXP indices) {
+	return variable_bit_get_rvector(bin, indices, fread_BIN);
+}
+
+SEXP pck_get_rvector(SEXP pck, SEXP indices) {
+	return variable_bit_get_rvector(pck, indices, fread_PCK);
+}
+
+SEXP chr_get_rvector(SEXP chr, SEXP indices) {
+	return variable_byte_get_rvector(
+		chr, indices,
+		[](std::fstream& f, size_t instance_len) {
+			std::string s = fread_fixed_string(f, instance_len);
+			rtrim(s);
+			return mkChar(s.c_str());
+		},
+		[](SEXP dst, int dst_index, SEXP s) {
+			SET_STRING_ELT(dst, dst_index, s);
+		},
+		NA_STRING, STRSXP
+	);
+}
+
 SEXP int_get_rvector(SEXP intg, SEXP indices) {
-	std::string path = CHAR(asChar(STRING_ELT(getAttrib(intg, mkString("data.path")), 0)));
-	const int instance_len = INTEGER_ELT(getAttrib(intg, mkString("instance.len")), 0);
-	const int instance_num = INTEGER_ELT(getAttrib(intg, mkString("instance.num")), 0);
-
-	std::fstream f (path, std::ios_base::in | std::ios_base::binary);
-	if (!f)
-		error_return("could not open data file for reading");
-
-	ProtectedSEXP ans = allocVector(INTSXP, length(indices));
-
-	for (int dst_index = 0; dst_index < length(indices); ++dst_index) {
-		int src_index = INTEGER_ELT(indices, dst_index)-1;
-
-		if (src_index < 0 or src_index >= instance_num) {
-			SET_INTEGER_ELT(ans, dst_index, NA_INTEGER);
-			continue;
-		}
-
-		f.seekg(src_index * instance_len);
-		uint16_t v = fread_uint16_t(f);
-		SET_INTEGER_ELT(ans, dst_index, v);
-	}
+	SEXP ans = variable_byte_get_rvector(
+		intg, indices,
+		[](std::fstream& f, int unused) { return fread_uint16_t(f);},
+		SET_INTEGER_ELT, NA_INTEGER, INTSXP
+	);
 	configure_factor(ans, intg);
 	return ans;
 }
 
 SEXP lng_get_rvector(SEXP lng, SEXP indices) {
-	std::string path = CHAR(asChar(STRING_ELT(getAttrib(lng, mkString("data.path")), 0)));
-	const int instance_len = INTEGER_ELT(getAttrib(lng, mkString("instance.len")), 0);
-	const int instance_num = INTEGER_ELT(getAttrib(lng, mkString("instance.num")), 0);
-
-	std::fstream f (path, std::ios_base::in | std::ios_base::binary);
-	if (!f)
-		error_return("could not open data file for reading");
-
-	ProtectedSEXP ans = allocVector(INTSXP, length(indices));
-
-	for (int dst_index = 0; dst_index < length(indices); ++dst_index) {
-		int src_index = INTEGER_ELT(indices, dst_index) - 1;
-
-		if (src_index < 0 or src_index >= instance_num) {
-			SET_INTEGER_ELT(ans, dst_index, NA_INTEGER);
-			continue;
-		}
-
-		f.seekg(src_index * instance_len);
-		uint32_t v = fread_uint32_t(f);
-		SET_INTEGER_ELT(ans, dst_index, v);
-	}
+	SEXP ans = variable_byte_get_rvector(
+		lng, indices,
+		[](std::fstream& f, int unused) { return fread_uint32_t(f);},
+		SET_INTEGER_ELT, NA_INTEGER, INTSXP
+	);
 	configure_factor(ans, lng);
 	return ans;
 }
 
 SEXP real_get_rvector(SEXP real, SEXP indices) {
-	std::string path = CHAR(asChar(STRING_ELT(getAttrib(real, mkString("data.path")), 0)));
-	const int instance_len = INTEGER_ELT(getAttrib(real, mkString("instance.len")), 0);
-	const int instance_num = INTEGER_ELT(getAttrib(real, mkString("instance.num")), 0);
-
-	std::fstream f (path, std::ios_base::in | std::ios_base::binary);
-	if (!f)
-		error_return("could not open data file for reading");
-
-	ProtectedSEXP ans = allocVector(INTSXP, length(indices));
-
-	for (int dst_index = 0; dst_index < length(indices); ++dst_index) {
-		int src_index = INTEGER_ELT(indices, dst_index)-1;
-
-		if (src_index < 0 or src_index >= instance_num) {
-			SET_REAL_ELT(ans, dst_index, NA_REAL);
-			continue;
-		}
-
-		f.seekg(src_index * instance_len);
-		double v = fread_double(f);
-		SET_REAL_ELT(ans, dst_index, v);
-	}
-	return ans;
+	return variable_byte_get_rvector(
+		real, indices,
+		[](std::fstream& f, int unused) { return fread_double(f);},
+		SET_REAL_ELT, NA_REAL, REALSXP
+	);
 }
 
 }
